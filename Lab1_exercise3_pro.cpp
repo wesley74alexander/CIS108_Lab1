f335e908d8d543ff92e9f5cd165af51ff567dd8e
wesley74alexander <46700007+wesley74alexander@users.noreply.github.com>
2019-01-22 19:39:29 -0800

    Update Lab1_exercise3_pro.cpp

--- Lab1_exercise3_pro.cpp
+++ Lab1_exercise3_pro.cpp
@@ -5,17 +5,18 @@ int main()
   int sum = 0, prod = 1;
   for(int num = 2; num < 1000; num++){
     bool prime = true;
-    for(int test = 2; test < num; test++){
+    for(int test = 2; test < num / 2; test++){
       if(num % test == 0){
         prime = false;
         break;
       }
-    else if(prime == true){
-      cout << num;
-      sum = sum + num;
-      prod = prod * num;
+      else if(prime == true){
+        cout << num;
+        sum += num;
+        prod *= num;
     }
     }
   }
-  cout << "the sum of all primes from 1-1000 is " << sum << " the product of all primes from 0-1000 is " << prod;
+  cout << "the sum of all primes from 1-1000 is " << sum << " the product of all primes from 0-1000 is " << prod << endl;
+  return 0;
 }
