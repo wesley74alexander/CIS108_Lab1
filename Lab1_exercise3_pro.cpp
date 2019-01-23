f0fda0a75c0b8733a2ee963dead04e5af520defb
Wesley Alexander <wesleyalexander@wesleys-mbp.home>
2019-01-22 16:54:51 -0800

    first commit

---
+++ Lab1_exercise3_pro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int sum = 0, prod = 1;
+  for(int num = 2; num < 1000; num++){
+    bool prime = true;
+    for(int test = 2; test < num; test++){
+      if(num % test == 0){
+        prime = false;
+        break;
+      }
+    else if(prime == true){
+      cout << num;
+      sum = sum + num;
+      prod = prod * num;
+    }
+    }
+  }
+  cout << "the sum of all primes from 1-1000 is " << sum << " the product of all primes from 0-1000 is " << prod;
+}
